38d2d9e52f2d3f9972ada5e8f9420aa8f9ba5e16
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-05-22 15:14:17 +0200

    inherit min-height

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -34,7 +34,7 @@ export default {
         const { value: variableValue, setValue } = wwLib.wwVariable.useComponentVariable({
             uid: props.uid,
             name: 'value',
-            defaultValue: computed(() => props.content.value === undefined ? '' : props.content.value),
+            defaultValue: computed(() => (props.content.value === undefined ? '' : props.content.value)),
         });
         const options = computed(() => {
             if (!props.content.options) return [];
@@ -154,6 +154,7 @@ export default {
         outline: none;
         width: inherit;
         height: inherit;
+        min-height: inherit;
         border: none;
         background-color: inherit;
         font-family: var(--input-fontFamily);
